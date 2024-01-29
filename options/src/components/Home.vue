bae73af18865f42ae72d7b90dbee2aa043bdcffe
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-01-29 21:53:48 +0000

    Añadiendo mejor comentario

--- options/src/components/Home.vue
+++ options/src/components/Home.vue
@@ -9,7 +9,11 @@
 
     export default {
         setup() {
-            //ref hace que la variables sea reactiva
+            //ref hace que la variables sea reactiva y es para
+            //las variables primitivas y se coge el valor
+            //usando .value
+            //reactive se usa para los objetos y hace que sea
+            //reactivo cada propiedad y eso no lo hace reactive
             const obj = reactive({ counter: 0});
 
             setInterval(() => obj.counter++, 1000);
