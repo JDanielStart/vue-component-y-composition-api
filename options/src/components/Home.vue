df4ae1dd3cbb4186c3ecd9a246d69f435070cca6
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-01-29 22:39:28 +0000

    Ejemplo de uso de computed en setup()

--- options/src/components/Home.vue
+++ options/src/components/Home.vue
@@ -1,28 +1,23 @@
 <template>
-    <div> {{ obj.counter }} </div>
+    <div> {{ fullName }} </div>
 </template>
 
 <script>
-
-    // eslint-disable-next-line no-unused-vars
-    import { reactive, watch } from 'vue';
+    import { ref, computed } from 'vue';
 
     export default {
         setup() {
-            //ref hace que la variables sea reactiva
-            const obj = reactive({ counter: 0});
 
-            setInterval(() => obj.counter++, 1000);
+            const firstName = ref("Diana");
+            const lastName = ref("Matínez");
 
-            watch(
-                () => obj.counter,
-                (valor, anterior) => {
-                console.log(valor, anterior);
+            const fullName = computed(() => {
+                return `${firstName.value} ${lastName.value}`;
             });
-            
+
             return {
-                obj
-            }
+                fullName
+            };
         }
     };
 </script>
